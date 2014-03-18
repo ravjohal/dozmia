7812d6fd8a0d4a58ab683419ccaba82af7f3bedb
ravjohal <ravneetjohal@gmail.com>
2014-03-18 01:36:14 -0700

    Add Users controller and test suite and also condensed

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
